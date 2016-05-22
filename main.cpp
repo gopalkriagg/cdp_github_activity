39a327589c8637ff2c845de3903fcf503ecf2f98
TusharGupta01 <tushar.behror@gmail.com>
2016-05-23 01:11:27 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "functions.h"
 using namespace std;
 int main(){
-    print_hello();
+    print_hello(); // hello
     cout<< endl;
     printf("The factorial of 5 is %d\n", factorial(5));
     return 0;
